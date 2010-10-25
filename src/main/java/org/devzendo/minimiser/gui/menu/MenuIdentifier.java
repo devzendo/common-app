45b85715135a626cf9b0f0d11e2bc5cf6acbfba3
Matt Gumbley <matt.gumbley@devzendo.org>
2010-10-25 07:54:19 +0100

    Make MenuIdentifier reusable: make the MenuIdentifier enum a top-level enum.

---
+++ src/main/java/org/devzendo/minimiser/gui/menu/MenuIdentifier.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (C) 2008-2010 Matt Gumbley, DevZendo.org <http://devzendo.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * 
+ */
+package org.devzendo.minimiser.gui.menu;
+
+/**
+ * Identifiers for the various menu items.
+ * 
+ * @author matt
+ *
+ */
+public enum MenuIdentifier {
+    /**
+     *
+     */
+    FileNew,
+    /**
+     *
+     */
+    FileOpen,
+    /**
+     *
+     */
+    FileClose,
+    /**
+     *
+     */
+    FileCloseAll,
+    /**
+     *
+     */
+    FileImport,
+    /**
+     *
+     */
+    FileExport,
+    /**
+     *
+     */
+    FileExit,
+
+    // Window menu is handled internally by the menu
+    // View menu is handled internally by the menu
+
+    /**
+     *
+     */
+    ToolsOptions,
+    /**
+     *
+     */
+    HelpWelcome,
+    /**
+     *
+     */
+    HelpWhatsNew,
+    /**
+     *
+     */
+    HelpAbout,
+    /**
+     *
+     */
+    HelpContents,
+    /**
+     *
+     */
+    HelpCheckForUpdates
+}
